cc064065da45c4e2ee7cf37aef7ad48997340b4a
João Barreto <jmmmbarreto@gmail.com>
2017-05-07 17:53:31 +0100

    falta uma ainda não está actualizada

--- 0106-helloworld.php
+++ 0106-helloworld.php
@@ -1,6 +1,7 @@
 <?php
 
     echo "O meu primeiro código PHP!<strong>Olá Mundo!</strong>";
+    
 
     echo "Estou a experimentar como se faz GitHub e o meu repositório ao meu portátil</strong>";
 
