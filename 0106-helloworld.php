6a394dfd839994454caed29e981eb9e6706d934f
Jo3Ba <jmmmbarreto@gmail.com>
2017-05-07 17:47:14 +0100

    Adicionei mais uma linha para experimentar.

--- 0106-helloworld.php
+++ 0106-helloworld.php
@@ -1,3 +1,5 @@
 <?php
-    echo "O meu primeiro código PHP!<strong>Olá Mundo!</strong>";
+    echo "O meu primeiro código PHP!<strong>Olá Mundo 123!</strong>";
+
+    echo "Estou a experimentar como se faz GitHub e o meu repositório ao meu portátil</strong>";
 ?>
